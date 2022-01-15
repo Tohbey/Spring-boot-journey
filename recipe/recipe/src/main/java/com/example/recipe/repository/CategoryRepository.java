162d869722d5738d3a83c5e999fe76c41e7ecdfa
tohbey <fafoworatobi25@gmail.com>
2022-01-15 19:37:45 +0100

    recipe repository modification

--- recipe/recipe/src/main/java/com/example/recipe/repository/CategoryRepository.java
+++ recipe/recipe/src/main/java/com/example/recipe/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.example.recipe.repository;
 import com.example.recipe.model.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
--- recipe/recipe/src/main/java/com/example/recipe/repository/UnitOfMeasureRepository.java
+++ recipe/recipe/src/main/java/com/example/recipe/repository/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.example.recipe.repository;
 import com.example.recipe.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
