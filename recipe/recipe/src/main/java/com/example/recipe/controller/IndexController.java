f1a38045fe48d93c723681e7878f23b97a6e4b9a
tohbey <fafoworatobi25@gmail.com>
2022-01-24 09:11:07 +0100

    index controller

--- recipe/recipe/src/main/java/com/example/recipe/controller/IndexController.java
+++ recipe/recipe/src/main/java/com/example/recipe/controller/IndexController.java
@@ -1,13 +1,23 @@
 package com.example.recipe.controller;
 
+import com.example.recipe.service.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService){
+        this.recipeService = recipeService;
+    }
 
     @RequestMapping({"/","","index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
