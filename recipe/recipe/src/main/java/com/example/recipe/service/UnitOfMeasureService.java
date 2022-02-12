535766bc736516f7467a9400da8f23f26f1d8e7e
tohbey <fafoworatobi25@gmail.com>
2022-02-13 00:37:53 +0100

    unit of measure service setup and test

---
+++ recipe/recipe/src/main/java/com/example/recipe/service/UnitOfMeasureService.java
@@ -0,0 +1,9 @@
+package com.example.recipe.service;
+
+import com.example.recipe.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+public interface UnitOfMeasureService {
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
---
+++ recipe/recipe/src/test/java/com/example/recipe/service/UnitOfMeasureServiceImplTest.java
@@ -0,0 +1,53 @@
+package com.example.recipe.service;
+
+import com.example.recipe.commands.UnitOfMeasureCommand;
+import com.example.recipe.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import com.example.recipe.model.UnitOfMeasure;
+import com.example.recipe.repository.UnitOfMeasureRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class UnitOfMeasureServiceImplTest {
+    UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand = new UnitOfMeasureToUnitOfMeasureCommand();
+    UnitOfMeasureService service;
+
+    @Mock
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        service = new UnitOfMeasureServiceImpl(unitOfMeasureRepository, unitOfMeasureToUnitOfMeasureCommand);
+    }
+
+    @Test
+    public void listAllUoms() throws Exception {
+        //given
+        Set<UnitOfMeasure> unitOfMeasures = new HashSet<>();
+        UnitOfMeasure uom1 = new UnitOfMeasure();
+        uom1.setId(1L);
+        unitOfMeasures.add(uom1);
+
+        UnitOfMeasure uom2 = new UnitOfMeasure();
+        uom2.setId(2L);
+        unitOfMeasures.add(uom2);
+
+        when(unitOfMeasureRepository.findAll()).thenReturn(unitOfMeasures);
+
+        //when
+        Set<UnitOfMeasureCommand> commands = service.listAllUoms();
+
+        //then
+        assertEquals(2, commands.size());
+        verify(unitOfMeasureRepository, times(1)).findAll();
+    }
+}
