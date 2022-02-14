37a1a077a316d27e285b12cec8c2899884324b24
tohbey <fafoworatobi25@gmail.com>
2022-02-14 08:55:53 +0100

    delete ingredient in recipe test

--- recipe/recipe/src/test/java/com/example/recipe/controller/IngredientControllerTest.java
+++ recipe/recipe/src/test/java/com/example/recipe/controller/IngredientControllerTest.java
@@ -92,6 +92,7 @@ public class IngredientControllerTest {
                 .andExpect(model().attributeExists("uomList"));
     }
 
+
     @Test
     public void testSaveOrUpdate() throws Exception {
         //given
@@ -110,6 +111,40 @@ public class IngredientControllerTest {
                 )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/ingredient/3/show"));
+
+    }
+
+    @Test
+    public void testNewIngredientForm() throws Exception {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        //when
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+        when(unitOfMeasureService.listAllUoms()).thenReturn(new HashSet<>());
+
+        //then
+        mockMvc.perform(get("/recipe/1/ingredient/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/ingredientform"))
+                .andExpect(model().attributeExists("ingredient"))
+                .andExpect(model().attributeExists("uomList"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
+
+    }
+
+    @Test
+    public void testDeleteIngredient() throws Exception {
+
+        //then
+        mockMvc.perform(get("/recipe/2/ingredient/3/delete")
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/2/ingredients"));
+
+        verify(ingredientService, times(1)).deleteIngredient(anyLong(), anyLong());
 
     }
 }
