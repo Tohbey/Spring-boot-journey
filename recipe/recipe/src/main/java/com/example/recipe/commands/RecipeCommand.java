754cc9ce713777e33e33910bfdd4994bd54956ed
tohbey <fafoworatobi25@gmail.com>
2022-02-15 15:33:35 +0100

    setting up validation and hibernate validator annotation

--- recipe/recipe/src/main/java/com/example/recipe/commands/RecipeCommand.java
+++ recipe/recipe/src/main/java/com/example/recipe/commands/RecipeCommand.java
@@ -5,7 +5,12 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,16 +19,33 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(100)
     private Integer servings;
-    private String url;
     private String source;
+
+    @URL
+    private String url;
+
+    @NotBlank
     private String directions;
+
+    private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
     private Difficulty difficulty;
     private NotesCommand notes;
     private Set<CategoryCommand> categories = new HashSet<>();
-    private Set<IngredientCommand> ingredients = new HashSet<>();
 }
--- recipe/recipe/src/test/java/com/example/recipe/controller/RecipeControllerTest.java
+++ recipe/recipe/src/test/java/com/example/recipe/controller/RecipeControllerTest.java
@@ -95,9 +95,27 @@ public class RecipeControllerTest {
                                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .param("id", "")
                     .param("description", "some string")
+                        .param("direction", "some directions")
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
+    }
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("id", "")
+                        .param("cookTime", "3000")
+                )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/recipeform"));
     }
 
     @Test
